e8bdac82d42f1fc6f9bf85c0553287e6fd2c3a55
edufruit <edufruit@gmail.com>
2015-08-28 20:12:29 +0530

    f

--- www/js/main.js
+++ www/js/main.js
@@ -5,49 +5,98 @@ document.addEventListener("deviceready", deviceReady, true);
 }
 
 	function checkPreAuth() {
-alert(2);
-		var form = $("#loginForm");
-		
+
+		alert('Checking if credentials already saved...');
+	
+	
 		if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) 
 		{
-			$("#username", form).val(window.localStorage["username"]);
-			$("#password", form).val(window.localStorage["password"]);
+			
+			$("#username").val(window.localStorage["username"]);
+			
+			$("#password").val(window.localStorage["password"]);
+			
 			handleLogin();
+		
 		}
 
 	}
-
-function handleLogin() {
-	alert('handling your login');
 	
-var form = $("#loginForm");
-//disable the button so we can't resubmit while we wait
-$("#submitButton",form).attr("disabled","disabled");
-var u = $("#username", form).val();
-var p = $("#password", form).val();
-console.log("click");
-if(u != '' && p!= '') {
-$.post("http://www.edufruit.com/app_customerdetails.php?method=login&returnformat=json", {username:u,password:p}, function(res) {
-//if(res == true) {
-if(res.result == true) {
-//store
-window.localStorage["username"] = u;
-window.localStorage["password"] = p;
-window.localStorage["customername"] = res.customername;
-//$.mobile.changePage("some.html");
-$("body").pagecontainer("change", "some.html", {  }); 
-} else {
-navigator.notification.alert("Your login failed", function() {});
-}
-$("#submitButton").removeAttr("disabled");
-},"json");
-} else {
-//Thanks Igor!
-navigator.notification.alert("You must enter a username and password", function() {});
-$("#submitButton").removeAttr("disabled");
-}
-return false;
-}
+
+	function handleLogin() 
+	{
+			
+		alert('handling your login');
+		 
+		//disable the button so we can't resubmit while we wait
+		
+		$("#submitButton").attr("disabled","disabled");
+		
+		var u = $("#username").val();
+		
+		var p = $("#password").val();
+		
+		console.log("click");
+
+
+		if(u != '' && p!= '') 
+		{
+		
+			alert('Authenticating Your Credentials...');
+									
+		
+			$.post("http://www.edufruit.com/app_customerdetails.php?method=login&returnformat=json", {username:u,password:p}, function(res) {
+			 
+			 
+			if(res.result == true) 
+			{
+									//store
+									window.localStorage["username"] = u;
+									
+									window.localStorage["password"] = p;
+									
+									window.localStorage["customername"] = res.customername;
+									
+									alert('answer='+res.customername);
+									
+									//$.mobile.changePage("some.html");
+									
+									$.mobile.pageContainer.pagecontainer('change', "some.html", {
+										
+									  transition: 'flow',
+									  
+									  reload    : true
+									  
+									});
+									
+									//$("body").pagecontainer("change", "some.html", {  }); 
+									
+									
+									
+			} 
+			else 
+			{
+				navigator.notification.alert("Your login failed", function() {});
+			}
+			
+			$("#submitButton").removeAttr("disabled");
+
+			},"json");
+			
+
+		} 
+		else 
+		{
+			//Thanks Igor!
+			navigator.notification.alert("You must enter a username and password", function() {});
+			
+			$("#submitButton").removeAttr("disabled");
+		
+		}
+		
+		return false;
+
+	}
 
 function deviceReady() {
 
